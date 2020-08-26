517c408d93eb11409575dda4ac0dabb55c8c043b
adeeba reaz <adeebarivera@outlook.com>
2020-08-26 21:09:26 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -5,7 +5,7 @@ class Doctor
     @name = name
     @@all << self
   end
-  def new_appointment(patient, date)
+  def new_appointment(date, patient)
     appointment = Appointment.new(date, patient, self)
     appointment
   end
