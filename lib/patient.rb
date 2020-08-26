70f410ce89501151634b4f8170a510aa90b85243
adeeba reaz <adeebarivera@outlook.com>
2020-08-26 20:40:11 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,25 @@
+class Patient
+  @@all = []
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def new_appointment(doctor, date)
+    appointment = Appointment.new(date, doctor, self)
+    appointment
+  end
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
+  def doctors
+    self.appointments.collects do |appointment|
+      appointment.doctor
+    end
+  end
+end
